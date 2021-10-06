eb48f4b191abc7e342926d9311f0132b810003a5
Illia Paliy <cattano285@gmail.com>
2021-10-06 13:41:57 +0300

    'added_posts'

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from . import views
 from django.urls import path, include
+from .views import PostListView
 
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
+    path('home/', PostListView.as_view(), name='home'),
 
     ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, redirect
-
+from django.views.generic import ListView, DetailView
+from users.models import User
+from .models import Post
 
 def home(request):
 	return render(request, 'blog/home.html')
 
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html'
+	context_object_name = 'posts'
+
+
+	def get_queryset(self):
+		return Post.objects.all()
